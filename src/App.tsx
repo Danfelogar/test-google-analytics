c4575ce5a123ffdc65e29eafc899336d2073bc4a
Daniel Felipe Polo Garcia <daniel.garcia@olsoft.com.co>
2023-09-11 09:22:44 -0500

    adding googlemaps

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@
 // load css globals
 
 // components
+import { GoogleMaps } from './components'
 
 function App () {
   return (
     <div>
       <h1>Vite + React</h1>
+      <GoogleMaps />
     </div>
   )
 }
